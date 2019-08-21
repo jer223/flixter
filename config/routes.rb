e3b05ac5a8414b0645f21c594842e00c65fc6a25
Jeremy Hall <jhall229@gmail.com>
2019-08-21 02:31:08 +0000

    Finished setting up carrierwave and switched uploader to S3 also set up Figaro

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
     end
-    resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create]
+    resources :courses, only: [:new, :create, :show, :photo] do
+      resources :sections, only: [:new, :create, :show, :photo]
     end  
   end  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
---
+++ db/migrate/20190818011713_create_photos.rb
@@ -0,0 +1,11 @@
+class CreatePhotos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :photos do |t|
+      t.text :caption
+
+      t.timestamps
+    end
+
+    add_index :photos 
+  end
+end
