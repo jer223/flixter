2cc58df485654aa56b77110deb660a1a92280fb6
Jeremy Hall <jhall229@gmail.com>
2019-08-27 20:45:05 +0000

    Reverted changes made to lessons controllers and routes.rb to fix heroku issue

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,6 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_lesson, only: [:show]
+#  before_action :require_authorized_for_current_section
 
   def show
   end
