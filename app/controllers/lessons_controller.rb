ec04aebe819112bca765ef811dbab4691386460b
Jeremy Hall <jhall229@gmail.com>
2019-08-28 18:54:07 +0000

    Setup pyments with stripe

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,18 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-#  before_action :require_authorized_for_current_section
+  before_action :require_enrolled_in_current_lesson, only: [:show]
 
   def show
   end
   
   private
+
+  def require_enrolled_in_current_lesson
+    unless current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to current_lesson.section.course, alert: 'Enroll to view content'
+    end
+
+  end
 
   helper_method :current_lesson
   def current_lesson
